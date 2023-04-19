46b0a2cd61f5fa00f2ba419e7a1c6635409a352c
wardaoi <adi@xhouse.ro>
2023-04-20 00:41:24 +0300

    Initial

---
+++ after/plugin/nerdtree.lua
@@ -0,0 +1,5 @@
+
+vim.keymap.set("n", "<leader>n", ":NERDTreeFocus<CR>");
+vim.keymap.set("n", "<C-t>", ":NERDTreeToggle<CR>");
+vim.keymap.set("n", "<C-f>", ":NERDTreeFind<CR>");
+
---
+++ lua/adrian/init.lua
@@ -0,0 +1,7 @@
+require('adrian.set')
+require('adrian.remap')
+require('adrian.packer')
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
